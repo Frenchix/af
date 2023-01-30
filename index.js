73d6013d6c8d3880e0bc956c6dc5c3e0d074cf46
Frenchix <bapf62@hotmail.com>
2023-01-30 10:26:21 +0100

    path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
+const path = require('path');
 const router = require('./routes/router.js');
 
 const port = process.env.PORT || 5002
